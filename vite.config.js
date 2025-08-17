4d03092ac69049874a25e8268685715eb4e415e7
yashsharma <ys7097569@gmail.com>
2025-08-17 21:29:50 +0530

    second  commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   
+  base: '/profile/',
+   build: {
+    outDir: 'dist',
+  },
+  
   plugins: [
     tailwindcss(),
   ],
